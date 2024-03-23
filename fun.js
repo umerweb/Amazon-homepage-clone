334a20138eb203ea2aea3dd7f62df936f495e5af
M. Umer <158821657+umerweb@users.noreply.github.com>
2024-03-23 15:33:10 +0500

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,25 @@
+const pp1 = () => {
+   let x = document.getElementById('popup-content');
+   x.style.display = 'flex';
+}
+
+const pp2 = () => {
+    let y = document.getElementById('popup-content');
+    y.style.display = 'none'
+}
+
+
+const ff1 = ()=> {
+    let a = document.getElementById('popup2');
+    a.style.display ='flex';
+}
+
+const ff2 = () => {
+    let b = document.getElementById('popup2');
+    b.style.display = 'none'
+}
+
+const he1 =() => {
+   let h = document.getElementById('hero-section')
+   h.style.backgroundImage= ''
+}
